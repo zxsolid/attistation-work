f8cc4effb7131987678be0216048fdfdff7ac006
zxsolid <stasbaydak@gmail.com>
2023-07-31 00:51:44 +0300

    final work

--- FinalWork#2/attistation-work/src/Main.java
+++ FinalWork#2/attistation-work/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Database database = new Database();
+        Menu menu = new Menu(database);
+        menu.displayMenu();
     }
 }
